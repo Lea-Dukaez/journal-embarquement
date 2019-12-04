c9ec10b721b2998b97ac33fede8e0ad79a3c903b
Léa DUKAEZ <lea.dukaez@gmail.com>
2019-12-04 18:03:05 +0800

    authentication login - signup - logout ok

--- js/login.js
+++ js/login.js
@@ -1,18 +1,93 @@
-// Modal-login - accordion
-const collapseHeader = document.querySelectorAll(".collapse-header");
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDaV1_deg05sDQgZRwqIuKRsLICW7ptf5E",
+  authDomain: "taiwan-blog-e2ac1.firebaseapp.com",
+  databaseURL: "https://taiwan-blog-e2ac1.firebaseio.com",
+  projectId: "taiwan-blog-e2ac1",
+  storageBucket: "taiwan-blog-e2ac1.appspot.com",
+  messagingSenderId: "254095991684",
+  appId: "1:254095991684:web:376224c1ecaec75d6133d1",
+  measurementId: "G-VGCLK8X0DF"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const auth = firebase.auth();
 
+
+
+// const 
+const modal = document.querySelector('#modal-login');
+const closeModal = document.querySelector('.close-modal');
+const iconsLogin = document.querySelectorAll('.login');
+const iconsLogout = document.querySelectorAll('.log-out');
+const accountsUser = document.querySelectorAll('.account');
+
+const collapseHeader = document.querySelectorAll(".collapse-header");
+const inputs = document.querySelectorAll(".collapse-body input");
+
+const newAccountPsw = document.querySelector('.new-psw');
+const btnSignup = document.getElementById("signupbtn");
+const btnLogin = document.getElementById("loginbtn");
+
+const sideNavBurger = document.querySelector('.side-nav-burger');
+
+
+// close - open modal
+// When the user clicks the button
+iconsLogin.forEach(icon => {
+  icon.onclick = () => {
+    modal.style.display = "block";
+    sideNavBurger.style.display = "none";
+  }
+});
+
+// When the user clicks on <span> (x), close the modal
+closeModal.onclick = () => {
+  modal.style.display = "none";
+  sideNavBurger.style.display = "inline-block";
+  newAccountPsw.style.borderColor = "initial";
+  collapseHeader.forEach(header => {
+    if(header.classList.contains("active")){
+      header.classList.toggle("active");
+      header.nextElementSibling.firstElementChild.reset()
+      header.nextElementSibling.style.maxHeight = null;
+    }
+});
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    sideNavBurger.style.display = "inline-block";
+    newAccountPsw.style.borderColor = "initial";
+    collapseHeader.forEach(header => {
+      if(header.classList.contains("active")){
+        header.classList.toggle("active");
+        header.nextElementSibling.firstElementChild.reset()
+        header.nextElementSibling.style.maxHeight = null;
+      }
+    });
+  }
+}
+
+// Modal-login - accordion
 collapseHeader.forEach((header,index) => {
 
   header.addEventListener("click", e =>{
     header.classList.toggle("active");
-
     const collapseBody = header.nextElementSibling;
+    collapseBody.firstElementChild.reset();
+
+    // close open form on click
     if (collapseBody.style.maxHeight) {
       collapseBody.style.maxHeight = null;
     } else {
       collapseBody.style.maxHeight = collapseBody.scrollHeight + "px";
     }
 
+    // close one form when the other one is open
     if(!index){
       collapseHeader[index+1].classList.toggle("active");
       const collapseBodyOne = collapseHeader[index+1].nextElementSibling;
@@ -22,45 +97,91 @@ collapseHeader.forEach((header,index) => {
       const collapseBodyTwo = collapseHeader[index-1].nextElementSibling;
       collapseBodyTwo.style.maxHeight = null;
     }
-
   });
 });
 
-// close modal
-// When the user clicks the button
-const modal = document.querySelector('#modal-login');
-const iconLogin = document.querySelector('.login');
-iconLogin.onclick = () => {
-  modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-const closeModal = document.querySelector('.close-modal');
-closeModal.onclick = () => {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = (event) => {
-  if (event.target === modal) {
-    modal.style.display = "none";
+// Form sign in / sign up
+// Sign Up
+btnSignup.addEventListener('click', () => {
+  newUserEmail = document.querySelector("input[name='newemail']").value;
+  newUserPsw = document.querySelector("input[name='newpsw']").value;
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/ ;
+  if(regex.test(newUserPsw)){
+    auth.createUserWithEmailAndPassword(newUserEmail, newUserPsw)
+      .then(() => {
+        newAccountPsw.style.borderColor = "initial";
+        btnSignup.parentNode.reset();
+        btnSignup.parentNode.parentNode.style.maxHeight = null;
+        modal.style.display = "none";
+        sideNavBurger.style.display = "inline-block";
+        console.log('user created')
+      })
+      .catch(err => console.log(err.message));
+  } else {
+    console.log('invalid password')
+    newAccountPsw.style.borderColor = "red"; 
   }
-}
 
 
-// regular expression form
-const inputs = document.querySelectorAll(".collapse-body input");
-const newAccountForm = document.querySelector('.new-account');
-const regexPsw = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/ ;
+});
 
-const validate = (field, regex) => {
-  console.log(regex.test(field.value));
-}
 
-newAccountForm.addEventListener('keyup', e => {
-  console.log(e);
-  console.log(e.target);
-  console.log(e.target.attributes.name.value);
-  validate(e.target, regexPsw);
+// Sign In
+btnLogin.addEventListener('click',() => {
 
+  userEmail = document.querySelector("input[name='email']").value;
+  userPsw = document.querySelector("input[name='psw']").value;
+  errorMsg = document.querySelector(".error");
+
+  auth.signInWithEmailAndPassword(userEmail,userPsw)
+    .then( () => {
+      btnLogin.parentNode.reset();
+      btnLogin.parentNode.parentNode.style.maxHeight = null;
+      modal.style.display = "none";
+      sideNavBurger.style.display = "inline-block";
+      errorMsg.style.display = "none";
+      console.log('user logged in');
+    })
+    .catch(err => {
+      console.log(err.message);
+      errorMsg.style.display = "block";
+    });
+});
+
+// log out users
+iconsLogout.forEach(icon => {
+  icon.addEventListener("click", () => {
+    auth.signOut();
+  });
+});
+
+
+// Add a realtime listener
+auth.onAuthStateChanged(firebaseUser => {
+  if(firebaseUser){
+    iconsLogout.forEach(icon => {
+      icon.style.display = "inline-block";
+    });
+    accountsUser.forEach(acc => {
+      acc.style.display = "inline-block";
+    });
+    iconsLogin.forEach(icon => {
+      icon.style.display = "none";
+    });
+    console.log(firebaseUser);
+  }else{
+    iconsLogout.forEach(icon => {
+      icon.style.display = "none";
+    });
+    accountsUser.forEach(acc => {
+      acc.style.display = "none";
+    });
+    iconsLogin.forEach(icon => {
+      icon.style.display = "block";
+    });
+    console.log("not logged in");
+  }
 });
